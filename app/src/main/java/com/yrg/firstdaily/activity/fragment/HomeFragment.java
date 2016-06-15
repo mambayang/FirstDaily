af1994061abd065e1094815ad5f51fd8899608a4
mambayang <jayfansyang@qq.com>
2016-06-15 14:45:15 +0800

    add top story viewpager

--- app/src/main/java/com/yrg/firstdaily/activity/fragment/HomeFragment.java
+++ app/src/main/java/com/yrg/firstdaily/activity/fragment/HomeFragment.java
@@ -9,14 +9,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.yrg.firstdaily.R;
 import com.yrg.firstdaily.adapter.TopStoriesPagerAdapter;
 import com.yrg.firstdaily.entity.TodayContent;
 import com.yrg.firstdaily.entity.TodayTopStory;
 import com.yrg.firstdaily.net.URLConstant;
 import com.yrg.firstdaily.util.GsonUtil;
+import com.yrg.firstdaily.util.ScreenUtil;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 
@@ -28,7 +30,7 @@ import okhttp3.Call;
 public class HomeFragment extends Fragment implements ViewPager.OnPageChangeListener {
 
     private static final String TAG = "HomeFragment";
-    private List<ImageView> layoutList;
+    private List<View> layoutList;
     private ViewPager vpTopStories;
 
     public HomeFragment() {
@@ -40,10 +42,10 @@ public class HomeFragment extends Fragment implements ViewPager.OnPageChangeList
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         vpTopStories = (ViewPager) view.findViewById(R.id.vp_top_stories);
+        layoutList = new ArrayList<>();
         getHomeData();
         return view;
     }
-
 
     private void getHomeData() {
         OkHttpUtils.get().url(URLConstant.URL_HOME_CONTENT).build().execute(new StringCallback() {
@@ -54,10 +56,8 @@ public class HomeFragment extends Fragment implements ViewPager.OnPageChangeList
 
             @Override
             public void onResponse(String response, int id) {
-                Log.i(TAG, response.toString());
                 ParseResponse(response);
                 setAdatper();
-
             }
         });
     }
@@ -75,7 +75,15 @@ public class HomeFragment extends Fragment implements ViewPager.OnPageChangeList
         for (int i = 0; i < topStories.size(); i++) {
             Log.i(TAG, topStories.get(i).getTitle());
             Log.i(TAG, topStories.get(i).getImage());
-            ImageView iv = new ImageView(getContext());
+            //创建view
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_top_story, null);
+            ImageView ivTopStoryImage = (ImageView) view.findViewById(R.id.iv_top_story_image);
+            TextView tvTopStoryTitle = (TextView) view.findViewById(R.id.tv_top_story_title);
+            //向view设置图片与标题
+            Picasso.with(getActivity()).load(topStories.get(i).getImage()).resize(ScreenUtil.getScreenWidth(getContext()), 240).centerCrop().into(ivTopStoryImage);
+            tvTopStoryTitle.setText(topStories.get(i).getTitle());
+            //保存到list中
+            layoutList.add(view);
         }
 
     }
